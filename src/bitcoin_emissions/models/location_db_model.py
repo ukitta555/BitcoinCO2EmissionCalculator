117646a1ee827f4e13151ecc14bd24b74bb57333
Vladyslav <ukitta555@gmail.com>
2024-06-10 18:12:35 -0400

    one more index

--- src/bitcoin_emissions/models/location_db_model.py
+++ src/bitcoin_emissions/models/location_db_model.py
@@ -4,6 +4,11 @@ from src.bitcoin_emissions.models.uuid_base_db_model import UUIDModel
 
 
 class Location(UUIDModel):
+    class Meta:
+        indexes = [
+            models.Index(fields=['location_name']),
+        ]
+
     location_name = models.CharField(max_length=200)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
